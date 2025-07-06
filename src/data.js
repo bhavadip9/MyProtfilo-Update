e017f2d13349bca8b0b28d1b93c1963780b1046e
Bhavadip Dhandhlya <bhavadip2004@gmail.com>
2025-07-06 18:11:22 +0530

    Add New Project

--- src/data.js
+++ src/data.js
@@ -1,6 +1,14 @@
 const projects = [
     {
         id: 1,
+        title: "Restaurant management system",
+        description: "A restaurant management system using .NET Core and PostgreSQL with features like user roles, menu setup, table booking, and order tracking. Added secure login with JWT and made the system fast and easy to use by automating key tasks. It helped improve service speed and gave me solid experience with backend development and real-world architecture.",
+        imageUrl: "/img/pizzashop.png",
+        link: "https://github.com/bhavadip9/Pizzashop",
+        technologies: [".Net Core", "PostgreSQL", "Html", "Bootstrap Css", "JQuery"],
+    },
+    {
+        id: 2,
         title: "E-commerce website",
         description: "Developed and maintained e-commerce websites using the MERN stack, improving user experience, payment integration,product management, and site performance. ",
         imageUrl: "/img/ecommerce.jpg",
@@ -8,7 +16,7 @@ const projects = [
         technologies: ["React.js", "Node.js", "React-bootstrap"]
     },
     {
-        id: 2,
+        id: 3,
         title: "Voice to text converter",
         description: "Build web applications using React js.",
         imageUrl: "/img/voicetotext.jpg",
@@ -16,7 +24,7 @@ const projects = [
         technologies: ["React.Js", "CSS"]
     },
     {
-        id: 3,
+        id: 4,
         title: "Books Store",
         description: "Books Store web application use for Upload and Read books and built with MERN Stack Development. ",
         imageUrl: "/img/BookStore.jpg",
@@ -24,7 +32,7 @@ const projects = [
         technologies: ["React", "Node.js", "Tailwind css"]
     },
     {
-        id: 4,
+        id: 5,
         title: "Signature App",
         description: "Build React web application using react-signature-canvas module.",
         imageUrl: "/img/Sign.jpg",
@@ -32,7 +40,7 @@ const projects = [
         technologies: ["React.Js", "react-signature-canvas module"]
     },
     {
-        id: 5,
+        id: 6,
         title: "Portfolit",
         description: "This my protflio website where I store all my project and my knowledge.",
         imageUrl: "/img/protfolio.jpg",
